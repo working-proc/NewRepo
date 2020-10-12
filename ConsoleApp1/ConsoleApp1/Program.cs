c3c8336152a65115999839e018e51876dee98888
Work <for_workingproc@mail.ru>
2020-10-12 16:45:05 +0400

    изменение кода для  создания конфликта

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
 
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Введите ваше имя");
             string name = Console.ReadLine();
             Console.WriteLine($"Hello World {name}!"); // добавление комментария (изменение комментария)
         }
