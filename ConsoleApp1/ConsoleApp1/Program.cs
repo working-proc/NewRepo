69bcaad460d0cf40fa1f61f0d435d0c92530fa89
Work <for_workingproc@mail.ru>
2020-10-12 15:34:38 +0400

    Добавление функционала

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using namespace std;
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string x = Console.ReadLine();
+            Console.WriteLine($"Hello World {x}!");
+        }
+    }
+}
